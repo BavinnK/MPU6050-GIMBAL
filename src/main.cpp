97e295ebdb131203c79e23379f2e455e9ce68da1
Bavin <bavinbav5@gmail.com>
2025-09-28 22:08:36 +0300

    tackled with three other more register CONFIG - GYRO - ACCEL registers

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,25 @@
+enum MPU_6050_registers{
+  PWR_MGMT_1=0x6B,
+  SMPLRT_DIV=0x19,
+  CONFIG=0x1A,
+  GYRO_CONFIG=0x1B,
+  ACCEL_CONFIG=0x1C
+};
 #include "MyI2C.h"
 #define mpu_addL 0x68 //when ADO pulled low 
 #define mpu_addH 0x69 //when ADO pulled high
 #define command_flag 0b00000000 //if RS is zero it means command 
 #define read_flag    0b00000001 //else if its 1 it means we want to display data
-#define PWR_MGMT_1 0x6B
+
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//PWR_MGMT_1 register mapping
 //Bit:  7              6     5      4          3      2    1    0
 //Name: DEVICE_RESET SLEEP CYCLE TEMP_DIS  Dontcare   CLKSEL[2:0]
+////////////////////////////
+//SMPLRT_DIV: 
+//where Gyroscope Output Rate = 8kHz when the DLPF is disabled (DLPF_CFG = 0 or 7), and 1kHz when the DLPF is enabled and by this formula you will get the smaple rate 
+//Sample Rate = Gyroscope Output Rate / (1 + SMPLRT_DIV)
+//and also we want the DLPF to be enabled all the time
 
 void mpu_init(){
   // first we have to wake the mpu from sleep and then we have to mess with it later
@@ -16,8 +29,54 @@ void mpu_init(){
   i2c_write(0b00000001); //as u can see the bit names of the PWR_MGMT_1 register we want to put everything zero but not the first bit why ? bc we use the gyro1 bc the datasheet says its recomended
   //Datasheet recommends using PLL with one of the gyros (usually X gyro → value 1) because it’s more stable than the internal 8mHz osc
   i2c_stop();
-}
 
+  i2c_start();
+  i2c_write(mpu_addL|command_flag);
+  i2c_write(CONFIG);
+  i2c_write(0b00000010);//as we said in above we have to enable the digital low pass filter and it depends on u but i wanted 100hz so closet option was third one in the table from the datasheet
+  i2c_stop();
+
+  i2c_start();
+  i2c_write(mpu_addL|command_flag);
+  i2c_write(SMPLRT_DIV);
+  i2c_write(0b00001001);//we want 100 hz why i explained it down in a comments
+  i2c_stop();
+  /*
+      What sample rate should you want?
+
+    It depends on your project:
+
+    Camera gimbal / robotics (your case) → 100 Hz to 200 Hz is perfect.
+
+    Fast enough to track motion smoothly
+
+    Slow enough that your microcontroller can handle the math
+
+    Common choice: 100 Hz (DIV=9)
+
+    Quadcopters with sensor fusion (MPU + filter/IMU) → Often 200–500 Hz. (e.g. DIV=1 → 500 Hz).
+
+    Simple tilt sensor / human motion → 50 Hz or even lower. (DIV=19 → 50 Hz).
+
+    High-speed experiments → Up to the max (1 kHz with DLPF, or 8 kHz without).
+  */
+  i2c_start();
+  i2c_write(mpu_addL|command_flag);
+  i2c_write(GYRO_CONFIG);
+  i2c_write(0b00000000);//we dont want any selftest so we dont care about the last 3 bits and the first three too all we care is the sensitivity of the gyro and we wanna set it to ± 250 °/s
+  //so we just sent a zero to that register and it should be fine
+  i2c_stop();
+
+
+  i2c_start();
+  i2c_write(mpu_addL|command_flag);
+  i2c_write(ACCEL_CONFIG);
+  i2c_write(0b00000000);//same as the gyro config register all we want is ± 2g then we send a zero to that register too
+  i2c_stop();
+
+
+}//done
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 
