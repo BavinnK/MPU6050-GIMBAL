4b0c3b42b21d9f10715aebe88fb9dbcaed2fd617
Bavin <bavinbav5@gmail.com>
2025-09-28 23:48:13 +0300

    now the init is finished and we got data back from the chip and i made a mistake the who am i reg was a readonly reg not write

--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,17 @@ enum MPU_6050_registers{
   GYRO_CONFIG=0x1B,
   ACCEL_CONFIG=0x1C,
   INT_ENABLE=0x38,
-  WHO_AM_I=0x75 //lol this is not a joke its acually one of the register names in MPU6050
+  WHO_AM_I=0x75 //lol this is not a joke its acually one of the register names in MPU6050 "its a readonly registerrrrrrrrrrrrrrrrrr"
 };
 #include "MyI2C.h"
+#include "MyUSART.h"
 #define mpu_addL 0x68 //when ADO pulled low 
 #define mpu_addH 0x69 //when ADO pulled high
 #define command_flag 0b00000000 //if RS is zero it means command 
 #define read_flag    0b00000001 //else if its 1 it means we want to display data
+#define clk_speed 16000000
+#define baud 9600
+#define my_ubrr (clk_speed/16/baud-1)
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //PWR_MGMT_1 register mapping
@@ -26,20 +30,20 @@ enum MPU_6050_registers{
 void mpu_init(){
   // first we have to wake the mpu from sleep and then we have to mess with it later
   i2c_start();
-  i2c_write(mpu_addL|command_flag);
+  i2c_write((mpu_addL<<1)|0);
   i2c_write(PWR_MGMT_1);
   i2c_write(0b00000001); //as u can see the bit names of the PWR_MGMT_1 register we want to put everything zero but not the first bit why ? bc we use the gyro1 bc the datasheet says its recomended
   //Datasheet recommends using PLL with one of the gyros (usually X gyro → value 1) because it’s more stable than the internal 8mHz osc
   i2c_stop();
 
   i2c_start();
-  i2c_write(mpu_addL|command_flag);
+  i2c_write((mpu_addL<<1)|0);
   i2c_write(CONFIG);
   i2c_write(0b00000010);//as we said in above we have to enable the digital low pass filter and it depends on u but i wanted 100hz so closet option was third one in the table from the datasheet
   i2c_stop();
 
   i2c_start();
-  i2c_write(mpu_addL|command_flag);
+  i2c_write((mpu_addL<<1)|0);
   i2c_write(SMPLRT_DIV);
   i2c_write(0b00001001);//we want 100 hz why i explained it down in a comments
   i2c_stop();
@@ -63,7 +67,7 @@ void mpu_init(){
     High-speed experiments → Up to the max (1 kHz with DLPF, or 8 kHz without).
   */
   i2c_start();
-  i2c_write(mpu_addL|command_flag);
+  i2c_write((mpu_addL<<1)|0);
   i2c_write(GYRO_CONFIG);
   i2c_write(0b00000000);//we dont want any selftest so we dont care about the last 3 bits and the first three too all we care is the sensitivity of the gyro and we wanna set it to ± 250 °/s
   //so we just sent a zero to that register and it should be fine
@@ -71,38 +75,52 @@ void mpu_init(){
 
 
   i2c_start();
-  i2c_write(mpu_addL|command_flag);
+  i2c_write((mpu_addL<<1)|0);
   i2c_write(ACCEL_CONFIG);
   i2c_write(0b00000000);//same as the gyro config register all we want is ± 2g then we send a zero to that register too
   i2c_stop();
 
   i2c_start();
-  i2c_write(mpu_addL|command_flag);
+  i2c_write((mpu_addL<<1)|0);
   i2c_write(INT_ENABLE);
   i2c_write(0b00000001);//we need only the data ready bit to be set to 1 for example the mot en bit when it senses motion it sends an int so the device wakes up but its used in power down mode so we dont want this
   //and the other ints too we dont want them we want continues data int
   i2c_stop();
 
 
-  i2c_start();
-  i2c_write(mpu_addL|command_flag);
-  i2c_write(WHO_AM_I);
-  i2c_write(0b01101000);
-  i2c_stop();
-  //so what is this who am i register 
-  /*
-  This register is used to verify the identity of the device. The contents of WHO_AM_I are the upper 6 bits of the MPU-60X0’s 7-bit I2 C address.
-   The least significant bit of the MPU-60X0’s I2C address is determined by the value of the AD0 pin. The value of the AD0 pin is not reflected in this register. straight from the datasheet
-  */
+  
 
 }//done
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+uint8_t mpu_read_register(uint8_t reg_addr) {
+    uint8_t data;
 
+    
+    i2c_start();
+    i2c_write((mpu_addL<<1)|0); 
+    i2c_write(reg_addr);
+    i2c_start();//repeated start 
+    i2c_write((mpu_addL<<1)|1);
+    data = i2c_read_nack(); 
+    i2c_stop();
+
+    return data;
+}
 
 
 void setup() {
   i2c_init();
+  mpu_init();
+  USART_init(my_ubrr);
+  //now we test if we tamed the beast or not lol
+  uint8_t who_am_i=mpu_read_register(WHO_AM_I);
+  if(who_am_i==mpu_addL){
+    USART_strTransmit("the beast has been tamed!!!!! :)");
+  }
+  else 
+    //Serial.println("the beast ran away!!!!! :( ");
+    USART_strTransmit("the beast ran away!!!!! :( ");
 
 }
 
